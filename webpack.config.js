a4fc42b1b10af4a1fd3c936dbd7a27efa514ed9c
Ferveloper <fermervil@gmail.com>
2019-02-03 23:06:03 +0100

    Basic interface design

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,68 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/entry.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-     resolve: {
-        extensions: ['.js'],
-    },
-    devtool: 'source-map',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'public/index.html')
-        })
-    ]
-}
+  devtool: 'source-map',
+  mode: 'development',
+  entry: './src/js/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      // {
+      //   test: /\.css$/,
+      //   use: ['style-loader',
+      //     {
+      //       loader: 'css-loader',
+      //       options: {
+      //         sourceMap: true,
+      //       },
+      //     },
+      //   ],
+      // },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/index.html'),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    open: true,
+    overlay: true,
+    port: 3000,
+    hot: true,
+    contentBase: [
+      path.join(__dirname, 'src'),
+      path.join(__dirname, 'src', 'templates'),
+    ],
+    watchContentBase: true,
+  },
+};
