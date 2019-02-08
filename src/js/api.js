552d2bd9fa57f921f8c934fd9063867c7e81e080
Ferveloper <fermervil@gmail.com>
2019-02-09 00:31:00 +0100

    JS extended

--- src/js/api.js
+++ src/js/api.js
@@ -1,7 +1,33 @@
+
+const API_KEY = 'API key';
+
 const api = (API_URL = 'http://api.tvmaze.com/') => {
   const SEARCH_API_URL = `${API_URL}search/shows?q=`;
   const SHOWS_URL = `${API_URL}shows`;
   return {
+    createQuote: async (id, text) => {
+      try {
+        const response = await fetch(`${API_URL}/${id}`, {
+          method: 'POST',
+          body: JSON.stringify({
+            quote: text,
+          }),
+          headers: {
+            'Content-type': 'application/json',
+            'X-API-KEY': API_KEY,
+          },
+        });
+        console.log(response);
+        if (!response.ok) {
+          throw 'Error';
+        }
+        const quote = await response.json();
+        return quote;
+      } catch (e) {
+        console.error(e);
+        throw e;
+      }
+    },
     getShows: async (query) => {
       try {
         const requestUrl = query ?
@@ -19,6 +45,15 @@ const api = (API_URL = 'http://api.tvmaze.com/') => {
       } catch (e) {
         console.error(e);
         throw e;
+      }
+    },
+    getShowsDetail: async (id) => {
+      try {
+        const response = await fetch(`${SHOWS_URL}/${id}`);
+        const show = await response.json();
+        return show;
+      } catch(e) {
+        console.error(e);
       }
     },
   };
